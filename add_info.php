47d0784b6afe50c441781f1d5f68fd31253e6153
jleong8 <jleong8@hawaii.edu>
2017-12-05 01:45:07 -1000

    Working add and read
    Functioning add and read need to add styling but it is functional

--- add_info.php
+++ add_info.php
@@ -2,28 +2,28 @@
 
 <?php
 
-  $rating = $_POST['rating'];
-  $username = $_POST['username'];
-  $distance = $_POST['distance'];
-  $voltage = $_POST['voltage'];
-  $amps = $_POST['dragtime'];
-  $temp = $_POST['temperature'];
-  $weather = $_POST['weather'];
-  $pressure = $_POST['pressure'];
-  $altitude = $_POST['altitude'];
+$rating = $_POST['rating'];
+$username = $_POST['username'];
+$distance = $_POST['distance'];
+$voltage = $_POST['voltage'];
+$amps = $_POST['dragtime'];
+$temp = $_POST['temperature'];
+$weather = $_POST['weather'];
+$pressure = $_POST['pressure'];
+$altitude = $_POST['altitude'];
+$dragtime = $_POST['dragtime'];
 
-  $sql = "INSERT INTO FattyCloudLeaderBoard (rating, userName, distance, voltage, amps, dragTime, temperature, weather, pressure, altitude) VALUES ($rating, $username, $distance, $voltage, $amps, $temp, $weather, $pressure, $altitude)"
-  $result = mysql_query($sql);
-  if(!$result) {
-    die('Data not entered:' . mysql_error());
-  }
-  echo "Data entered\n";
+$sql = "INSERT INTO FattyCloudLeaderboard (rating, userName, distance, voltage, amps, dragTime, temperature, weather, pressure, altitude) VALUES ('".$rating."', '".$username."', '".$distance."', '".$voltage."', '".$amps."', '".$dragtime."', '".$temp."', '".$weather."', '".$pressure."', '".$altitude."')";
+$result = mysql_query($sql, $conn);
+echo $sql;
+echo $result;
 
 ?>
 
 <html>
 <form id="create" action="add_info.php" method="POST">
   rating: <input type="text" name="rating"><br>
+  <!--Username input field is temporary this field should be populated based off user login info--->
   Username: <input type="text" name="username"><br>
   Distance: <input type="text" name="distance"><br>
   Voltage: <input type="text" name="voltage"><br>
@@ -35,4 +35,4 @@
   Altitude: <input type="text" name="altitude"><br>
   <input type="submit" name="submit">
 </form>
-    </html>
+</html>
